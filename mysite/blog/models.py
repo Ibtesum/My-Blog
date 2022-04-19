602ca490d3d3926dadec9b304d19d55bd8eebfd9
Ibtesum Reza Aninda <aninda7152@gmail.com>
2022-04-20 04:52:27 +0600

    Chapter 01 Completed

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -23,6 +27,14 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
         max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager() #the default manager
+    published = PublishedManager() # our custom manager
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year, 
+                            self.publish.month,
+                            self.publish.day, self.slug])
 
     class Meta:
         ordering = ('-publish',)
